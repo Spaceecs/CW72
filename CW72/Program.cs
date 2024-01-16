3b3dec27d0931a6880db22dd722546623238d8e8
Spaceecs <azaza2046goi@gmail.com>
2024-01-16 20:41:43 +0200

    for

--- CW72/Program.cs
+++ CW72/Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine(5 + 6);
+int a=0;
+for(int i=0;i<11;++i)
+{
+    Console.WriteLine(a);
+    ++a;
+}
